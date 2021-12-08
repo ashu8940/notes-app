979056126e3c2968c6d38b233308b192ee673af4
Ashutosh Dubey <dubeyashutosh306@gmail.com>
2021-12-08 20:17:45 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
-
+app.use(express.json());
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
