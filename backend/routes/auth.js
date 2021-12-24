3511dcd116a7b37688a8187a46a5fd00d8e382eb
Ashutosh Dubey <dubeyashutosh306@gmail.com>
2021-12-24 14:11:49 +0530

    added auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,15 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const User = require('../models/Users');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { json } = require('express/lib/response');
+
+const JWT_SCREAT='ashuisagoodb$oy'
+
 
 router.post('/createuser', [
-  body('name', 'Enter a valod name').isLength({ min: 3 }),
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', 'enter a valid email').isEmail(),
   body('password', 'password must be atleast 5 charcters').isLength({ min: 5 })],
   async (req, res) => {
@@ -20,19 +26,45 @@ router.post('/createuser', [
       if (user) {
         return res.status(400).json({ error: "sorry user already exsits with email" })
       }
+  const salt= await bcrypt.genSalt(10);
+ const secpass=await bcrypt.hash(req.body.password, salt);
+      // create User
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secpass,
         email: req.body.email
       })
-      res.json(user);
+const data={user:{
+  id:user.id,
+}
+}
+  const authToken =  jwt.sign(data,JWT_SCREAT);
+
+      res.json({authToken:authToken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occurred");
     }
-    //.then(User => res.json(User))
-    //.catch(err =>{ console.log(err);
-    // res.json({errors: "please enter a valid email address"})
+    
   })
+  router.post('/login', [
+   
+    body('email', 'enter a valid email').isEmail(),
+    body('password', 'password cannot be blank').exists()],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+ const {email, password} = req.body;
+try{
+  let user= User.findOne({ email: email})
+  if (!user){
+    return res.status(400).json({ error: " invalid credentials " })
+  }
+}catch(err){
+
+}
+    })
 
 module.exports = router
